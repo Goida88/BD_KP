a478a48240c105237e75df98450e1ac02c1ccea5
Goida88 <153019764+Goida88@users.noreply.github.com>
2024-12-20 22:59:17 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import asyncpg
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from app.handlers import router, set_db_pool
+from config2 import create_all_tables
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -21,8 +22,12 @@ async def create_db_pool():
     )
     set_db_pool(db_pool)
 
+async def setup_database():
+    await create_all_tables()
+
 async def main():
     await create_db_pool()
+    await setup_database()
     dp.include_router(router)
     await dp.start_polling(bot)
 
